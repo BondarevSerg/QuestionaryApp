06dd6cea701e00f5759fa99d53e40a07f7fdcd80
Sergey Bondarev <bondarevsy163@gmail.com>
2023-03-09 18:05:37 +0300

    add Mapstruct

--- src/main/java/ru/bondarev/questionary/mapper/QuestionMapper.java
+++ src/main/java/ru/bondarev/questionary/mapper/QuestionMapper.java
@@ -1,104 +1,25 @@
 package ru.bondarev.questionary.mapper;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import ru.bondarev.questionary.dto.request.AnswerRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import ru.bondarev.questionary.dto.request.QuestionRequest;
-import ru.bondarev.questionary.dto.response.AnswerResponse;
 import ru.bondarev.questionary.dto.response.QuestionResponse;
-import ru.bondarev.questionary.entity.Answer;
 import ru.bondarev.questionary.entity.Question;
-import ru.bondarev.questionary.entity.Quiz;
-import ru.bondarev.questionary.repositories.QuizRepository;
-import ru.bondarev.questionary.service.Imp.AnswerServiceImp;
-import ru.bondarev.questionary.service.Imp.QuestionServiceImp;
-import ru.bondarev.questionary.service.Imp.QuizServiceImp;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+@Mapper(uses = { AnswerMapper.class })
+public interface QuestionMapper {
+    QuestionMapper INSTANCE = Mappers.getMapper( QuestionMapper.class );
+   @Mapping(source = "id", target = "id")
+    @Mapping(source = "quiz.id", target = "quizId")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "answers", target = "answerResponseList")
+    QuestionResponse toDTO(Question question);
+    @Mapping(source = "quizId", target = "quiz.id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "answerRequestList", target = "answers")
+    Question toEntity(QuestionRequest questionRequest);
 
-/**
- * маппер вопросов
- */
-@Service
-@RequiredArgsConstructor
-public class QuestionMapper {
-
-
-    /**
-     * вопрос из энтити в dto
-     *
-     * @param question
-     * @return
-     */
-    public QuestionResponse entityToResponse(Question question) {
-        return QuestionResponse.builder()
-                .id(question.getId())
-                .title(question.getTitle())
-                .quizId(question.getQuiz().getId())
-                .answerResponseList(question.getAnswers().stream()
-                        .map(a -> AnswerResponse.builder()
-                                .id(a.getId())
-                                .title(a.getTitle())
-                                .questionId(a.getQuestion().getId())
-                                .build())
-                        .collect(Collectors.toList()))
-                .build();
-    }
-
-    /**
-     * список вопросов  из энтити в dto
-     *
-     * @param questions
-     * @return
-     */
-    public List<QuestionResponse> entityToResponse(List<Question> questions) {
-        if (!questions.isEmpty()) {
-            return questions.stream()
-                    .map(this::entityToResponse)
-                    .collect(Collectors.toList());
-        }
-
-        return Collections.emptyList();
-    }
-
-    /**
-     * ответ из dto в энтити
-     *
-     * @param questionRequest
-     * @return вопрос
-     */
-    public Question requestToEntity(QuestionRequest questionRequest) {
-        return Question.builder()
-                .id(questionRequest.getId())
-                .title(questionRequest.getTitle())
-                .quiz(Quiz.builder()
-                        .id(questionRequest.getQuizID())
-                        .build())
-                .answers(questionRequest.getAnswerRequestList().stream()
-                        .map(a -> Answer.builder()
-                                .id(a.getId())
-                                .build())
-                        .collect(Collectors.toList()))
-                .build();
-    }
-
-
-    /**
-     * список вопросов  из dto в энтити
-     *
-     * @param questions
-     * @return
-     */
-    public List<Question> requestToEntityList(List<QuestionRequest> questions) {
-        if (!questions.isEmpty()) {
-            return questions.stream()
-                    .map(this::requestToEntity)
-                    .collect(Collectors.toList());
-        }
-
-        return Collections.emptyList();
-    }
 
 }
+
